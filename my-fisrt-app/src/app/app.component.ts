bb5a65d36f9e33e97c8cc64ea678c1b6e4debacf
yuta <9010atuy@gmail.copm>
2020-04-05 23:08:52 +0900

    typescript grammy
    type, array

--- my-fisrt-app/src/app/app.component.ts
+++ my-fisrt-app/src/app/app.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-fisrt-app';
 
-  codeby = 'yuta';
+  codeby:string = 'yuta';
+  
+  // type
+  year:number = 2020;
+  month:number = 4;
+  day:number = 5;
+  // codeby:any = 'yuta';
+
+  // array
+  greets = ['Good Morning', 'Hello', 'Good Evening'];
+
+
+
 }
