afe32e08d3176b1a65fe53765fb071e39165d3cc
yuta <9010atuy@gmail.copm>
2020-04-14 20:57:24 +0900

    submit
    add routing top link
    add form
    add onSubmit
    add itemCart chage items

--- my-fisrt-app/src/app/item-shipping/item-shipping.component.ts
+++ my-fisrt-app/src/app/item-shipping/item-shipping.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit } from '@angular/core';
 import {ItemCartService} from '../itemcart.service';
-import { timingSafeEqual } from 'crypto';
 
 @Component({
     selector: 'app-item-shipping',
--- my-fisrt-app/src/app/itemcart/item-cart.component.ts
+++ my-fisrt-app/src/app/itemcart/item-cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ItemCartService } from "../itemcart.service";
+import { FormBuilder } from "@angular/forms";
+
 @Component({
     selector: 'app-item-cart',
     templateUrl: './item-cart.component.html',
@@ -8,12 +10,26 @@ import { ItemCartService } from "../itemcart.service";
 
 export class ItemCartComponent implements OnInit {
     items;
+    checkOutForm;
     constructor(
-        private itemCartService: ItemCartService
-    ) {};
+        private itemCartService: ItemCartService,
+        private formBuilder: FormBuilder
+    ) {
+        this.checkOutForm = this.formBuilder.group({
+            name: '',
+            address: ''
+        })
+    };
 
     ngOnInit() {
         this.items = this.itemCartService.getItems();
+    }
+
+    onSubmit(customerData) {
+        this.items = this.itemCartService.clearItemCart();
+        this.checkOutForm.reset();
+
+        console.warn('Your order has been submit!');
     }
 }
 
